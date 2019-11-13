4205941fb96ebd2f74e12ba290d4fb9c9310609e
iBlurBlur <tanakorn.ngam@gmail.com>
2019-11-13 18:11:04 +0700

    ...

--- my_map/lib/main.dart
+++ my_map/lib/main.dart
@@ -21,43 +21,50 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
   @override
   Widget build(BuildContext context) {
-
-
-
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.grey,
+                height: 150,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 170.0, right: 20),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Column(
+                    children: <Widget>[
+                      FloatingActionButton(
+                        child: Icon(Icons.map),
+                        onPressed: (){},
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        child: FloatingActionButton(
+                          child: Icon(Icons.map),
+                          onPressed: (){},
+                        ),
+                      ),
+                      FloatingActionButton(
+                        child: Icon(Icons.my_location),
+                        onPressed: (){},
+                      )
+                    ],
+                  ),
+                ),
+              )
+
+            ],
+          ),
+        ],
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
